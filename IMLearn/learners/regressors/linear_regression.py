5cd438e209c57e07e951b43529b43465f40710aa
nedved <nadavleder@gmail.com>
2023-04-06 18:53:11 +0300

    linear_regression.py finished

--- IMLearn/learners/regressors/linear_regression.py
+++ IMLearn/learners/regressors/linear_regression.py
@@ -3,6 +3,7 @@ from typing import NoReturn
 from ...base import BaseEstimator
 import numpy as np
 from numpy.linalg import pinv
+from ...metrics import mean_square_error
 
 
 class LinearRegression(BaseEstimator):
@@ -49,7 +50,7 @@ class LinearRegression(BaseEstimator):
         -----
         Fits model with or without an intercept depending on value of `self.include_intercept_`
         """
-        raise NotImplementedError()
+        self.coefs_ = pinv(np.insert(X, 0, 1, axis=1) if self.include_intercept_ else X) * y
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -65,7 +66,7 @@ class LinearRegression(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        raise NotImplementedError()
+        return (np.insert(X, 0, 1, axis=1) if self.include_intercept_ else X) * self.coefs_
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -84,4 +85,4 @@ class LinearRegression(BaseEstimator):
         loss : float
             Performance under MSE loss function
         """
-        raise NotImplementedError()
+        return ((np.insert(X, 0, 1, axis=1) if self.include_intercept_ else X) * self.coefs_ - y) ** 2
